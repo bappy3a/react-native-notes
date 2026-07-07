ae7e65142d97d81d9281c73b89c498a93843926a
Ahmed Bappy <bappy3a@gmail.com>
2026-07-08 01:31:00 +0600

    update

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
+    <View className="flex-1 items-center justify-center bg-[#1e293b]">
+      <Text className="text-xl font-bold text-red-500">
         Welcome to Nativewind!
       </Text>
     </View>

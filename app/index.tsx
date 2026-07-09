a250d0362a5b8d8628eb5de90c035ce03e2c7d90
Ahmed Bappy <bappy3a@gmail.com>
2026-07-09 16:31:03 +0600

    category show

--- app/index.tsx
+++ app/index.tsx
@@ -5,24 +5,43 @@ import SafeAreaView from "./components/SafeAreaView";
 
 export default function Index() {
   const [searchText, setSearchText] = useState("");
+  const [selectedCategoyr, setSelectedCategoyr] = useState("");
 
+  const handelCategoryClick = (category: string) => () => {
+    setSelectedCategoyr(category);
+  };
 
   return (
     <SafeAreaView className="flex-1 p-4 bg-alabaster">
       <View className="flex-1">
         <TextInput className="bg-white border border-gray-300 h-14 rounded-full p-4 w-full" placeholder="Search your thoughts..." placeholderTextColor="#9CA3AF" value={searchText} onChangeText={setSearchText} />
+
+        <ScrollView horizontal showsHorizontalScrollIndicator={false} style={{
+          minHeight: 50, maxHeight: 50, marginTop: 10, }} 
+          contentContainerStyle={{
+            flexDirection: "row",
+            gap: 2,
+            alignItems: "center",
+            paddingHorizontal: 4,
+          }}
+          >
+          {CATEGORYS.map((category) => {
+            const isSelected = selectedCategoyr === category;
+
+            return <Pressable key={category} style={{
+              paddingVertical: 10,
+              paddingHorizontal: 28,
+              borderRadius: 999,
+              backgroundColor: isSelected ? "#3838a3" : "#fff",
+              borderWidth: 1,
+              borderColor: isSelected ? "#3838a3" : "#fff",
+              marginRight: 8,}} onPress={handelCategoryClick(category)} ><Text style={{ color: isSelected ? "#fff" : "#3838a3" }}>{category}</Text></Pressable>
+          })}
+        </ScrollView>
+
       </View>
 
-      <ScrollView horizontal style={{
-        minHeight: 50,maxHeight: 50, marginTop:16
 
-      }} >
-        {CATEGORYS.map((category) => {
-          const isSelected = true;
-
-          return <Pressable className="pr-4 border border-gray-500 " key={category}><Text>{category}</Text></Pressable>
-        })}
-      </ScrollView>
 
     </SafeAreaView>
   );

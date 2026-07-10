d5e3d7e3d72cbb0d236f7b574b6f3cf938af518b
Ahmed Bappy <bappy3a@gmail.com>
2026-07-10 23:49:47 +0600

    update

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,35 @@
 import useNoteStore from "@/store/useNotes";
 import { CATEGORYS } from "@/utils/constants";
 import { AntDesign } from "@expo/vector-icons";
-import { useRoute } from "@react-navigation/native";
-import React, { useState } from "react";
+import { router } from "expo-router";
+import React, { useMemo, useState } from "react";
 import { FlatList, Pressable, ScrollView, Text, TextInput, View } from "react-native";
 import NoteCard from "./components/NoteCard";
 import SafeAreaView from "./components/SafeAreaView";
 
 export default function Index() {
-  const router = useRoute();
   const [searchText, setSearchText] = useState("");
   const [selectedCategoyr, setSelectedCategoyr] = useState("");
   const {notes} = useNoteStore();
 
 
   const handelCategoryClick = (category: string) => () => {
-    setSelectedCategoyr(category);
+    setSelectedCategoyr((prev)=> (prev === category ? "" : category) );
   };
+
+  const filteredNotes = useMemo(() => {
+    let result = notes;
+
+    if (selectedCategoyr) {
+      return result.filter((note) => note.category === selectedCategoyr);
+    }
+
+    if(searchText.trim()){
+      result = result.filter((note) => note.title.toLowerCase().includes(searchText.toLowerCase()));
+    }
+
+    return result;
+  }, []);
 
   return (
     <SafeAreaView className="flex-1 p-4 bg-alabaster">
@@ -40,21 +53,26 @@ export default function Index() {
               paddingVertical: 10,
               paddingHorizontal: 28,
               borderRadius: 999,
-              backgroundColor: isSelected ? "#3838a3" : "#fff",
+              backgroundColor: isSelected ? "#3838a3" : "#ededed",
               borderWidth: 1,
-              borderColor: isSelected ? "#3838a3" : "#fff",
+              borderColor: isSelected ? "#3838a3" : "#ededed",
               marginRight: 8,
             }} onPress={handelCategoryClick(category)} ><Text style={{ color: isSelected ? "#fff" : "#3838a3" }}>{category}</Text></Pressable>
           })}
         </ScrollView>
 
-        <FlatList
+        {filteredNotes.length === 0 ? <View className="w-full items-center mt-28 justify-center">
+          <Text className="text-medium text-2xl text-gray-400" >No return found</Text>
+          <Text>Tap + to comture your fist thougth</Text>
+        </View> : <FlatList
           data={notes}
           renderItem={({ item }) => <NoteCard note={item} />}
           keyExtractor={(item) => item.id.toString()}
           contentContainerStyle={{ paddingBottom: 100 }}
           showsVerticalScrollIndicator={false}
-        />
+        /> }
+
+        
 
         <Pressable onPress={() => router.push("/add")}  className="bg-governor-bay absolute bottom-8 right-8 w-16 h-16 rounded-3xl flex items-center justify-center">
           <AntDesign name="plus" size={24} color="white" />
